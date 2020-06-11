f822e35ca9b41a05176070c6b10f2d8d49b6f1ab
Victor <victorfgcabeceira@gmail.com>
2020-06-11 11:46:50 -0300

    Exercise 5 with async testing

--- exercises/testing/test.js
+++ exercises/testing/test.js
@@ -1,4 +1,11 @@
-// write some tests
+const { fixId, findUser } = require('./users');
+
 describe('users', () => {
-  
-})
+  test('fixId', () => {
+    expect(fixId('20')).toBe(20);
+  });
+
+  test('findUser', async () => {
+    await expect(findUser('0')).resolves.toEqual(expect.objectContaining({ id: 0 }));
+  });
+});
